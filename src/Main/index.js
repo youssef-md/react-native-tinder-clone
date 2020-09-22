0ed396d7e65c299669ad3b87e00bb637db03d004
youssef-md <emaildeyoussefmuhamad@gmail.com>
2020-09-21 22:48:18 -0300

    Interpolate swipe to generate rotate and load infinite dogs

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { Animated, PanResponder } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Animated, PanResponder, TouchableOpacity, Text } from 'react-native';
 import { width } from '../../utils/deviceInfo';
 
 import Card from '../Card';
@@ -14,36 +14,44 @@ export default function Main() {
   const swipe = useRef(new Animated.ValueXY()).current;
   const [pets, setPets] = useState(petsObj);
 
-  const panResponder = PanResponder.create({
-    onMoveShouldSetPanResponder: () => true,
-    onPanResponderMove: (e, { dx, dy }) => {
-      swipe.setValue({ x: dx, y: dy });
-    },
-    onPanResponderRelease: (e, { dx, dy }) => {
-      const direction = Math.sign(dx);
-      const userAction = Math.abs(dx) > ACTION_OFFSET;
+  useEffect(() => {
+    if (pets.length === 0) {
+      setPets(petsObj);
+    }
+  }, [pets]);
 
-      if (userAction) {
-        Animated.timing(swipe, {
-          duration: 200,
-          toValue: {
-            x: direction * CARD_OUT_WIDTH,
-            y: dy,
-          },
-          useNativeDriver: true,
-        }).start(transitionNext);
-      } else {
-        Animated.spring(swipe, {
-          friction: 5,
-          toValue: {
-            x: 0,
-            y: 0,
-          },
-          useNativeDriver: true,
-        }).start();
-      }
-    },
-  });
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (e, { dx, dy, y0 }) => {
+        swipe.setValue({ x: dx, y: dy });
+      },
+      onPanResponderRelease: (e, { dx, dy }) => {
+        const direction = Math.sign(dx);
+        const userAction = Math.abs(dx) > ACTION_OFFSET;
+
+        if (userAction) {
+          Animated.timing(swipe, {
+            duration: 200,
+            toValue: {
+              x: direction * CARD_OUT_WIDTH,
+              y: dy,
+            },
+            useNativeDriver: true,
+          }).start(transitionNext);
+        } else {
+          Animated.spring(swipe, {
+            friction: 5,
+            toValue: {
+              x: 0,
+              y: 0,
+            },
+            useNativeDriver: true,
+          }).start();
+        }
+      },
+    })
+  ).current;
 
   const transitionNext = useCallback(() => {
     setPets((prevState) => prevState.slice(1));
@@ -61,8 +69,14 @@ export default function Main() {
     [swipe.x, transitionNext]
   );
 
+  const rotate = swipe.x.interpolate({
+    inputRange: [-100, 0, 100],
+    outputRange: ['10deg', '0deg', '-10deg'],
+    extrapolate: 'clamp',
+  });
+
   const animatedCardStyle = {
-    transform: [...swipe.getTranslateTransform()],
+    transform: [...swipe.getTranslateTransform(), { rotate: rotate && rotate }],
   };
 
   return (
